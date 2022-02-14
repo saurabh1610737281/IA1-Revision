734295fcc7e0fb94814cb47ee5d4262bec1faa5b
saurabh16107372 <95605060+saurabh1610737281@users.noreply.github.com>
2022-02-14 05:41:44 +0000

    coding

---
+++ p1final.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+int input()
+{
+    int a; 
+    printf("Enter a number\n");
+    scanf("%d",&a);
+    return a;
+}
+
+int find_sum(int a, int b)
+{
+    int sum;
+    sum = a+b;
+    return sum;
+}
+
+void output(int a, int b, int c)
+{
+    printf("Sum of %d + %d is %d\n",a,b,c);
+}
+
+int main()
+{
+    int x,y,z;
+    x=input();
+    y=input();
+    z=find_sum(x,y);
+    output(x,y,z);
+    return 0;
+}
+
+
+
+
+
+
+
+
